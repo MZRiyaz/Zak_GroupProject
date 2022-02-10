3d3ff89f64691a9f82c32e6ba292aa1458add53b
zriyaz <zakriariyaz@gmail.com>
2022-02-09 21:51:34 -0500

    change to code

--- app/src/main/java/com/example/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/myyearbook/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myyearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    @SuppressLint("SetTextI18n")
+    public void buttonClick(View view)
+{
+TextView ChangedText = (TextView)findViewById(R.id.textView2);
+ChangedText.setText("Text has been changed");
+}
+
 }
